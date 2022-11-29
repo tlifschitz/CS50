ab01ce460b57c560bcaf51c335a8f0f4eef5f537
Tobias <tlifschitz@itba.edu.ar>
2022-11-29 02:53:11 -0300

    Implement Add and Random skeleton

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("add", views.add, name="add"),
+    path("random", views.rand_entry, name="random"),
     path("<str:entry_name>", views.entry, name="entry")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from . import util
-
+import random
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -9,16 +10,24 @@ def index(request):
     })
 
 def entry(request, entry_name):
-    entry = util.get_entry(entry_name)
+    content = util.get_entry(entry_name)
 
-    if entry is not None:
+    if content is not None:
         return render(request, "encyclopedia/entry.html", {
             "entry_name": entry_name,
-            "entry" : entry
+            "entry" : content
         })
     else:
         return render(request, "encyclopedia/entry-not-found.html", {
             "entry_name": entry_name
         })
+
+def add(request):
+    return render(request, "encyclopedia/add.html")
+
+def rand_entry(request):
+    if request.method == "GET":
+        entry_name = random.choice(util.list_entries())
+        return redirect(reverse(entry, args=[entry_name]))
 
 
